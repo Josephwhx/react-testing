27007905ef0aa556cd33216967128824a711e9a2
Josephwhx <JosephWonghx@gmail.com>
2023-03-28 17:46:22 +0800

    Test and code functions and table rows

---
+++ users/src/UserForm.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('it shows two inputs and a button', async () => {
+    const user = userEvent.setup();
+    // render the component
+    render(<UserForm onUserAdd={jest.fn()}/>);
+
+    // Manipulate the component or find an element in it
+    const allInputs = screen.getAllByRole('textbox');
+    const allButton = screen.getAllByRole('button');
+
+    // Assertion - make sure the component is doing what we expect it to do
+    expect(allInputs).toHaveLength(2);
+    expect(allButton).toHaveLength(1);
+});
+
+test('it calls onUserAdd when the form is submitted', async () => {
+    // mock function
+    const mock = jest.fn();
+    const user = userEvent.setup();
+    // render component
+    render(<UserForm onUserAdd={jest.fn()}/>);
+
+    // Find the two inputs
+    const nameInput = screen.getByRole('textbox', {name: 'Name'});
+    const emailInput = screen.getByRole('textbox', {name: 'Email'});
+
+    // Simulate typing in a name
+    await user.clear(nameInput);
+    await user.type(nameInput, "asdf");
+
+    // Simulate typing in an email
+    await user.clear(emailInput);
+    await user.type(emailInput, "asdf@email.com");
+
+    // Find the button
+    const button = screen.getByRole('button');
+
+    // Simulate clicking the button
+    await user.click(button);
+
+    // Assertion to make sure 'onUserAdd' is called
+    expect(mock).toHaveBeenCalled(1);
+    expect(mock).toHaveBeenCalledWith({name: 'asdf', email: 'asdf@email.com'});
+});
---
+++ users/src/UserList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function UserList({users}){
+    const rederedUsers = users.map((user) => {
+        return (
+        <tr key={user.name}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+        </tr>
+        );
+    });
+    
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody data-testid="users">{rederedUsers}</tbody>
+        </table>
+    )
+}
