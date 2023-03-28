8f60f5d69ac814d91aa47940ac49d585c1443823
Josephwhx <JosephWonghx@gmail.com>
2023-03-28 17:58:44 +0800

    revert back to data-testid

--- users/src/UserList.test.js
+++ users/src/UserList.test.js
@@ -7,15 +7,10 @@ test('render one row per user', () => {
         {name: 'jane', email: 'jane@jane.com'},
         {name: 'sam', email: 'sam@sam.com'}
     ];
-    const {container} = render(<UserList users={users}/>);
+    render(<UserList users={users}/>);
 
     // Find all the rows in the table
-    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-    const rows = container.querySelectorAll('tbody tr');
-
-    // using data-testid
-    //--------------------------------------
-    // const rows = within(screen.getByTestId('users')).getAllByRole('row');
+    const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
     // Assertion: correct number of rows in the table
     expect(rows).toHaveLength(2);
